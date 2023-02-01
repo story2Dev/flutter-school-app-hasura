200ad4a30310777868df6119e28b5788e7385339
Mark AI <chanthavong_12@hotmail.com>
2023-02-01 09:22:20 +0700

    add login form ui

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  void loginAction() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Sign in your account'),
+                const SizedBox(height: 10),
+                Text(
+                  'View score and exam',
+                  style: TextStyle(
+                    fontSize: Theme.of(context).textTheme.titleLarge?.fontSize,
+                    color: Colors.purple,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 40),
+            Align(
+              child: Image.network(
+                'https://cdn-icons-png.flaticon.com/512/3429/3429402.png',
+                height: 120.0,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                label: Text('Email'),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                label: Text('Password'),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Transform.translate(
+                      offset: const Offset(-10, 0),
+                      child: Radio(
+                        value: false,
+                        groupValue: false,
+                        onChanged: (isCheck) {},
+                      ),
+                    ),
+                    const Text('Remember me'),
+                  ],
+                ),
+                SizedBox(
+                    child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Forgot password?'),
+                ))
+              ],
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: loginAction,
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.all(15)),
+                  elevation: MaterialStateProperty.all(2.0),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.purple),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: const BorderSide(color: Colors.purple)),
+                  ),
+                ),
+                child: const Text('Login'),
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
